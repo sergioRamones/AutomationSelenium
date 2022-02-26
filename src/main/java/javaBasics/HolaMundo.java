1b1bceb7c5db650ab977ab9dda29d04231b7910f
sergioRamones <srv444@hotmail.com>
2022-02-26 11:18:25 -0600

    Clases ejemplo java basico

---
+++ src/main/java/javaBasics/HolaMundo.java
@@ -0,0 +1,65 @@
+package javaBasics;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class HolaMundo {
+	
+	int sumatoria = 10;
+	static int suma = 0;
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Hola Mundo");
+		
+	}
+	
+	@Test(priority=2)
+	public void showConsole() {
+		System.out.println("Hola Mundo");
+		Reporter.log("Hola Mundo",true);
+		sumatoria = sumatoria + 1;
+		Reporter.log("La sumatoria es:" + sumatoria,true);
+		
+	}
+	
+	@Test(priority=1)
+	public void typeOfVariables() {
+		int numero1 = 10;
+		char letra = 's';
+		float numeroDecimail = 10.5f;
+		double numeroGrande = 10.2387492387;
+		boolean flag = true;
+		String nombre = "Nohemi";
+		Boolean flag2=false;
+		Double numeroGrande2 = 20.872937129379812738;
+		int numero2;
+		sumatoria = numero1 + sumatoria;
+		Reporter.log("La sumatoria es: " + sumatoria,true);
+		Reporter.log("El valor de la variable: " + numero1,true);
+	}
+	
+	@Test(priority=3)
+	public void operacionesAritmeticas() {
+		int numero1 = 10;
+		int numero2 = 99;
+		double resultado = 0;
+		
+		resultado = numero1+numero2;
+		Reporter.log("El resultado de la suma es: " + resultado,true);
+		
+		resultado = numero1 - numero2;
+		Reporter.log("El resultado de la resta es: " + resultado,true);
+		
+		resultado = numero1*numero2;
+		Reporter.log("El resultado de la aplicacion es: " + resultado,true);
+		
+		resultado = numero2/numero1;
+		Reporter.log("El resultado de la division es: " + resultado,true);
+		
+		resultado = numero2%numero1;
+		Reporter.log("El resultado del residuo es: " + resultado,true);
+		
+	}
+
+}
---
+++ src/main/java/javaBasics/promedioDeCalificaciones.java
@@ -0,0 +1,102 @@
+package javaBasics;
+
+import java.util.Scanner;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class promedioDeCalificaciones {
+	
+	double cal1=24;
+	double cal2=90;
+	double cal3=87;
+	double cal4=500;
+	
+	@Test
+	public void calcularPromedio() {
+		double resultado = (cal1+cal2+cal3+cal4)/4;
+		
+//		Reporter.log("El resultado del promedio es: [" + resultado + "]",true);
+		
+		if(resultado>=70 && resultado<=100) {
+			Reporter.log("Estas aprobado con: [" + resultado + "]",true);
+			
+		}else if(resultado<70 && resultado>=0) {
+			
+			Reporter.log("Estas reprobado con: [" + resultado+ "]",true);
+		}else {
+			Reporter.log("Las calificaciones estan fuera de ranngo, promedio [" + resultado + "]",true);
+		}
+		
+		if(resultado>=70 || resultado<=100) {
+			Reporter.log("OR Estas aprobado con: [" + resultado + "]",true);
+			
+		}
+	}//End Calcular Promedio
+	
+	@Test
+	public void calcularPromedioPor100Teclado() {
+		Scanner reader = new Scanner(System.in);
+		
+		do {
+		Reporter.log("Ingresa la calificacion 1",true);
+		cal1=reader.nextDouble();
+		
+		Reporter.log("Ingresa la calificacion 2",true);
+		cal2=reader.nextDouble();
+		}while(cal1 <0 || cal1 >100 || cal2 <0 || cal2 >100);
+		
+		Reporter.log("El promedio de las calificaciones es: " + (cal1+cal2)/2,true);
+		double resultado = resultadoPromedio(cal1,cal2);
+		calcularPromedio(resultado);
+		double result = calcularPromedioRetorno(87);
+		Reporter.log("El valor de retorno es: " + result,true);
+		
+	}//Por Teclado
+	
+	public void calcularPromedio(double resultado) {
+		
+		Reporter.log("El resultado del promedio es: [" + resultado + "]",true);
+		
+		if(resultado>=70 && resultado<=100) {
+			Reporter.log("Estas aprobado con: [" + resultado + "]",true);
+			
+		}else if(resultado<70 && resultado>=0) {
+			
+			Reporter.log("Estas reprobado con: [" + resultado+ "]",true);
+		}else {
+			Reporter.log("Las calificaciones estan fuera de ranngo, promedio [" + resultado + "]",true);
+		}
+		
+		if(resultado>=70 || resultado<=100) {
+			Reporter.log("OR Estas aprobado con: [" + resultado + "]",true);
+			
+		}
+	}
+	
+public double calcularPromedioRetorno(double resultado) {
+		
+		Reporter.log("El resultado del promedio es: [" + resultado + "]",true);
+		
+		if(resultado>=70 && resultado<=100) {
+			Reporter.log("Estas aprobado con: [" + resultado + "]",true);
+			
+		}else if(resultado<70 && resultado>=0) {
+			
+			Reporter.log("Estas reprobado con: [" + resultado+ "]",true);
+		}else {
+			Reporter.log("Las calificaciones estan fuera de ranngo, promedio [" + resultado + "]",true);
+		}
+		
+		if(resultado>=70 || resultado<=100) {
+			Reporter.log("OR Estas aprobado con: [" + resultado + "]",true);
+			
+		}
+		return resultado+23;
+	}//CalcularPromedioRetorno
+
+	public double resultadoPromedio(double num1, double num2) {
+		return (num1+num2/2);
+	}
+
+}
