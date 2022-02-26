bceb40bb4bd75b6ffac8c50d8466378d56824e7b
sergioRamones <srv444@hotmail.com>
2022-02-26 11:53:55 -0600

    Tarea + clases ejemplo

--- readme.txt
+++ readme.txt
@@ -1,1 +1,7 @@
 Este es un repositorio de un entrenamiento de automatizacion
+
+Ejercicios:
+
+	1.Programa que lea dos numeros por telcado y que evalue si son igulaes o si un numero es mayor que el otro.
+	2.Programa que lea dos numeros por teclados pero se debe repetir hasta que los numeros sean iguales utlizar
+	ciclo do-while.
---
+++ src/main/java/javaBasics/Ciclos.java
@@ -0,0 +1,90 @@
+package javaBasics;
+
+import java.util.Scanner;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Ciclos {
+	
+	
+	
+	@Test
+	public void ciclos() {
+		
+		int index = 0;
+		int numero = 10;
+		
+		for(int i=0; i<=numero; i++) {
+			
+			Reporter.log("El valor de nuestro indice es FOR: " + i,true);
+			
+		}
+		
+		System.out.println("\n");
+		while(index<=numero) {
+			
+			Reporter.log("El valor de nuestro indice es WHILE: " + index,true);
+			index++;
+		}
+		
+		
+	}//end ciclo
+	
+	@Test
+	public void condicionalSwitch() {
+		Scanner reader = new Scanner(System.in);
+		Reporter.log("Ingresa un numero de mes", true);
+		String mes = reader.next();
+		reader.close();
+		
+		switch (mes) {
+		case "1":
+			Reporter.log("Enero", true);
+			break;
+		case "2":
+			Reporter.log("Febrero", true);
+			break;
+		case "3":
+			Reporter.log("Marzo", true);
+			break;
+		case "4":
+			Reporter.log("Abril", true);
+			break;
+		case "5":
+			Reporter.log("Mayo", true);
+			break;
+		case "6":
+			Reporter.log("Junio", true);
+			break;
+		case "7":
+			Reporter.log("Julio", true);
+			break;
+		case "8":
+			Reporter.log("Agosto", true);
+			break;
+		case "9":
+			Reporter.log("Septiembre", true);
+			break;
+		case "10":
+			Reporter.log("Octubre", true);
+			break;
+		case "11":
+			Reporter.log("Noviembre", true);
+			break;
+		case "12":
+			Reporter.log("Diciembre", true);
+			break;
+			
+			default:
+				Reporter.log("El numero que ingresaste no corresponde a ningun mes [ "+ mes+" ]", true);
+			
+			
+		}//end switch
+		
+		
+		
+		
+	}
+
+}//end class
